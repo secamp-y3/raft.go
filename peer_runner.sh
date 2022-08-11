51ef8c830df622d57ddab5a933f2656094bab7c1
logica <84759675+logica0419@users.noreply.github.com>
2022-08-11 14:12:55 +0900

    fixed path

--- peer_runner.sh
+++ peer_runner.sh
@@ -10,4 +10,4 @@ fi
 
 name=$(printf 'peer%02d' "$peer")
 date=$(date '+%Y%m%d_%H:%M:%S')
-go run peer.go --name "$name" --port $((peer + 3000)) --dispatcher "$dispatcher" >log/"${name}"_"${date}".log 2>&1
+go run cmd/peer/main.go --name "$name" --port $((peer + 3000)) --dispatcher "$dispatcher" >log/"${name}"_"${date}".log 2>&1
