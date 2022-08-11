535e8403f6c8191f9f965583554cc709825b748c
logica <84759675+logica0419@users.noreply.github.com>
2022-08-11 14:10:25 +0900

    format sh

--- peer_runner.sh
+++ peer_runner.sh
@@ -5,9 +5,9 @@ peer=$1
 dispatcher=$2
 
 if [ ! -d log ]; then
-    mkdir log
+  mkdir log
 fi
 
-name=$(printf 'peer%02d' $peer)
+name=$(printf 'peer%02d' "$peer")
 date=$(date '+%Y%m%d_%H:%M:%S')
-go run peer.go --name $name --port $(($peer + 3000)) --dispatcher $dispatcher > log/${name}_${date}.log 2>&1
+go run peer.go --name "$name" --port $((peer + 3000)) --dispatcher "$dispatcher" >log/"${name}"_"${date}".log 2>&1
